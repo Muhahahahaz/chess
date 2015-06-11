d1aca1a91145e48a266213ee801e6a670f8eb08d
Spencer Shepard <spencer.shepard@gmail.com>
2015-06-11 03:27:21 -0700

    Added basic README and changed code indentation style.

---
+++ README.md
@@ -0,0 +1,3 @@
+# chess
+
+My first attempt at writing a chess engine... Bear with me. :-P
--- main.c
+++ main.c
@@ -2,35 +2,35 @@
 #include <stdlib.h>
 
 void check(long i) {
-	if(i == 1)
-		return;
+  if(i == 1)
+    return;
 
-	long x = i;
+  long x = i;
 
-	while(1) {
-		if(i % 2 == 0)
-			i /= 2;
-		else
-			i = (3*i + 1) / 2;
+  while(1) {
+    if(i % 2 == 0)
+      i /= 2;
+    else
+      i = (3*i + 1) / 2;
 
-		if(i < x)
-			return;
-	}
+    if(i < x)
+      return;
+  }
 }
 
 int main(int argc, char *argv[]) {
-	if(argc != 3) {
-		fprintf(stderr, "Error! Usage: serial <START> <STOP>\n");
-		return -1;
-	}
+  if(argc != 3) {
+    fprintf(stderr, "Error! Usage: serial <START> <STOP>\n");
+    return -1;
+  }
 
-	long i = atol(argv[1]), max = atol(argv[2]);
+  long i = atol(argv[1]), max = atol(argv[2]);
 
-	for( ; i <= max; i++) {
-		check(i);
-	}
+  for( ; i <= max; i++) {
+    check(i);
+  }
 
-	printf("Success! All i <= %ld checked out.\n", max);
+  printf("Success! All i <= %ld checked out.\n", max);
 
-	return 0;
+  return 0;
 }
